149b9fe20c26a1701e591c96acf91e1afef79045
jmartinezcode <jmartinez.data@gmail.com>
2023-11-07 12:46:20 -0600

    [2.01] Created AddPost to DbContext

--- DbContext.cs
+++ DbContext.cs
@@ -11,6 +11,12 @@ public class BloggingContext : DbContext
         this.Blogs.Add(blog);
         this.SaveChanges();
     }
+    
+    public void AddPost(Post post)
+    {
+        this.Posts.Add(post);
+        this.SaveChanges();
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
